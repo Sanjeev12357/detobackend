13556effb10792ad531a78cc9594bb125d4610a1
Sanjeev Singh <sanjeevsinghsaini48@gmail.com>
2025-06-02 09:22:34 +0530

    updated bacckend queery

--- routes/user.js
+++ routes/user.js
@@ -21,7 +21,7 @@ userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
     const page = parseInt(req.query.page) || 1;
-    let limit = parseInt(req.query.limit) || 10;
+    let limit = parseInt(req.query.limit) || 20;
     limit = limit > 50 ? 50 : limit;
     const pageSkip = (page - 1) * limit;
 
